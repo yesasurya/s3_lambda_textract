db665399aed3c8cebf0bf70cef0c83c17320ef19
Yesa <yesa@rumah.com>
2021-07-19 00:23:24 +0700

    Update configuration

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 DEFAULT_REGION = 'us-east-1'
 
 ## The name of S3 bucket to store CSV files
-S3_BUCKET_FOR_CSV = 'upworkbucket4csv'
+S3_BUCKET_FOR_CSV = ''
 
 ## True: CSV containing data with associated type will be generated
 ## False: CSV containing data with associated type will NOT be generated
